aad3a15b94f71a45c1a6fbd4ab2af20df76a821f
trishnaeiysys <trishna@eiysys.in>
2023-01-06 11:49:21 +0530

    second

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+        <img width="100%" height="100%" src="https://images.unsplash.com/photo-1585409677983-0f6c41ca9c3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80" alt="" />
+    </div>
+  )
+}
+
+export default about
