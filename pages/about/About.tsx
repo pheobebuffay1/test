7804ca6be8a8d6bb833ba9aebac3ac9cddd87128
trishnaeiysys <trishna@eiysys.in>
2023-01-06 12:06:48 +0530

    third

--- pages/about/About.tsx
+++ pages/about/About.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const about = () => {
+const About = () => {
   return (
     <div>
         <img width="100%" height="100%" src="https://images.unsplash.com/photo-1585409677983-0f6c41ca9c3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80" alt="" />
@@ -8,4 +8,4 @@ const about = () => {
   )
 }
 
-export default about
+export default About
